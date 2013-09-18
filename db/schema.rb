5f3b999eb35c6f12f85922ed424e4bfbea42030f
lithium <code@hlidskialf.com>
2013-09-18 16:08:57 -0700

    Simple devise user tests

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130917024156) do
+ActiveRecord::Schema.define(version: 20130918222731) do
 
   create_table "rails_admin_histories", force: true do |t|
     t.text     "message"
@@ -43,5 +43,17 @@ ActiveRecord::Schema.define(version: 20130917024156) do
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+
+  create_table "work_histories", force: true do |t|
+    t.string   "company"
+    t.string   "position"
+    t.date     "start_date"
+    t.date     "end_date"
+    t.string   "address"
+    t.text     "contact_info"
+    t.text     "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,8 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should not save without an email" do
+    user = User.new
+    assert !user.save, "Saved a user without an email"
+  end
 end
