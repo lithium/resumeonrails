ea11cc0adacda2c958e8005168ece4b3ec0dc27c
lithium <code@hlidskialf.com>
2013-09-18 21:01:56 -0700

    Consistent unit test names.

--- test/models/work_history_test.rb
+++ test/models/work_history_test.rb
@@ -1,28 +1,28 @@
 require 'test_helper'
 
 class WorkHistoryTest < ActiveSupport::TestCase
-  test "shouldn't save without company" do
+  test "don't save without company" do
     work_history = WorkHistory.new
     work_history.position = "Pencil Pusher"
     work_history.start_date = Date.new(2001,1,1)
     refute work_history.save, "Saved a WorkHistory without company"
   end
 
-  test "shouldn't save without position" do
+  test "don't save without position" do
     work_history = WorkHistory.new
     work_history.company = "Acme"
     work_history.start_date = Date.new(2001,1,1)
     refute work_history.save, "Saved a WorkHistory without a position"
   end
 
-  test "shouldn't save without start_date" do
+  test "don't save without start_date" do
     work_history = WorkHistory.new
     work_history.company = "Acme"
     work_history.position = "Pencil Pusher"
     refute work_history.save, "Saved a WorkHistory without a start_date"
   end
 
-  test "shouldnt save with overlapping dates" do
+  test "don't save with overlapping dates" do
     WorkHistory.create(company: "Acme", 
                        position: "Pencil Puser", 
                        start_date: Date.new(2001,1,1), 
