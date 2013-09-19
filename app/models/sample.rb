399581688fe3010dfc2276b64af872b548e9546b
lithium <code@hlidskialf.com>
2013-09-18 22:30:03 -0700

    Fix uri validation for empty string.

--- app/models/sample.rb
+++ app/models/sample.rb
@@ -1,14 +1,15 @@
 class Sample < ActiveRecord::Base
+  has_attached_file :screenshot, :styles => { :small => "150x150>" }
 
   validates :name, uniqueness: true, presence: true
   validate :source_url_parses_if_present
 
   def source_url_parses_if_present
-    if source_url
+    if source_url.to_s != '' # not nil or empty
       valid_url = false
       begin
         uri = URI.parse(source_url)
-        valid_url = true if uri.host 
+        valid_url = true if uri.scheme && uri.path 
       rescue URI::InvalidURIError => e
       end
       errors.add(:source_url, "Does not seem like a valid URL") unless valid_url
--- test/models/sample_test.rb
+++ test/models/sample_test.rb
@@ -19,6 +19,7 @@ class SampleTest < ActiveSupport::TestCase
     assert Sample.create(:name => "One").valid?, "Couldn't save a Sample with an empty url!"
     refute Sample.create(:name => "Two", :source_url => "waka").valid?, "Saved a Sample with an invalid source_url!"
     assert Sample.create(:name => "Three", :source_url => "https://github.com/").valid?, "Couldn't save a Sample with a valid source_url!"
+    assert Sample.create(:name => "four", :source_url => "").valid?, "Unable to save a Sample with an empty string for source_url!"
   end
 
 end
