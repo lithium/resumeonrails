38accf889e97c0c0f9beb1e61a5539109f1b0fe5
lithium <code@hlidskialf.com>
2013-09-20 14:38:10 -0700

    work history name and scope cleanup

--- app/models/work_history.rb
+++ app/models/work_history.rb
@@ -1,5 +1,11 @@
 class WorkHistory < ActiveRecord::Base
 
+  default_scope { order("start_date") }
+
+  def name
+    "#{position} - #{company}"
+  end
+
   validates_presence_of :company, :position, :start_date
   validate :start_date_cannot_overlap
 
@@ -7,6 +13,7 @@ class WorkHistory < ActiveRecord::Base
     overlapping = WorkHistory.where("start_date <= ? AND end_date >= ?", start_date, start_date)
     errors.add(:start_date, "overlaps existing Work History") if overlapping.first
   end
+
 
   rails_admin do 
     edit do
