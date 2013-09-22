e68442d4e85c66e6a381f07eaf6ae2d9fced4ba8
lithium <code@hlidskialf.com>
2013-09-21 19:50:39 -0700

    anchor support on load

--- app/assets/javascripts/pages.js
+++ app/assets/javascripts/pages.js
@@ -6,17 +6,30 @@ $(function() {
   })
 
   $('.sidebar-nav a').on('click', function() {
-    var $this = $(this);
-    var $parent = $this.parents('li');
-    $parent.siblings().removeClass('active');
-    $parent.addClass('active');
-    var href = $this.attr('href').replace(/^#/,'')
-    var $target = $('div[name="'+href+'"]')
+    scroll_to_name($(this).attr('href'))
+  })
+
+  if (window.location.hash) {
+    scroll_to_name(window.location.hash)
+  }
+
+})
+
+
+function scroll_to_name(name)
+{
+    var name = name.replace(/^#/,'');
+    var $target = $('div[name="'+name+'"]');
+    var $link = $('a[href="#'+name+'"]');
+    var $parent = $link.parent();
+    $parent.siblings().removeClass('active')
+    $parent.addClass('active')
+
+    var $pane = $target.parents('.tab-pane')
+    $('.navbar-fixed-top a[href="#'+$pane[0].id+'"]').tab('show');
 
     $('html, body').animate({
       scrollTop: $target.offset().top-50
     }, 500)
 
-  })
-
-})
+}
