11a7db372c38aeadba676883f9e0531f8511a6f8
lithium <code@hlidskialf.com>
2013-09-22 16:28:04 -0700

    add empty check to optional layout field

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,7 +5,8 @@ class PagesController < ApplicationController
     @current_page = Page.find_by slug: slug
     raise ActionController::RoutingError.new('Page Not Found') unless @current_page
 
-    layout = @current_page.layout ? @current_page.layout : "resume"
+    layout = @current_page.layout
+    layout = "resume" if layout.empty? or !layout 
 
 
     render layout: layout
