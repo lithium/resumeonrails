2648035b3516fd0755fb45616779085395a8b45e
lithium <code@hlidskialf.com>
2013-09-22 18:08:52 -0700

    case insensitive slug dispatch

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,7 @@ class PagesController < ApplicationController
 
   def page
     slug = "/#{params[:slug]}"
-    @current_page = Page.find_by slug: slug
+    @current_page = Page.find(:first, :conditions => ["lower(slug) = ?", slug.downcase])
     raise ActionController::RoutingError.new('Page Not Found') unless @current_page
 
     layout = @current_page.layout
